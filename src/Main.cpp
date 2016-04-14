4f8818c9235a6a4aa5acdfb38556858b56936300
astewa44 <astewa44@utk.edu>
2016-04-14 09:00:43 -0400

    initial commit

---
+++ src/Main.cpp
@@ -0,0 +1,53 @@
+#include <SFML/Graphics.hpp>
+#include <pathfinder.h>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    int POINT_LENGTH = 3;
+
+    Waypoint *points = (Waypoint*)malloc(sizeof(Waypoint) * POINT_LENGTH);
+
+    Waypoint p1 = { -4, -1, d2r(45) };      // Waypoint @ x=-4, y=-1, exit angle=45 degrees
+    Waypoint p2 = { -1, 2, 0 };             // Waypoint @ x=-1, y= 2, exit angle= 0 radians
+    Waypoint p3 = {  2, 4, 0 };             // Waypoint @ x= 2, y= 4, exit angle= 0 radians
+    points[0] = p1;
+    points[1] = p2;
+    points[2] = p3;
+    
+    TrajectoryCandidate candidate;
+    pathfinder_prepare(points, POINT_LENGTH, FIT_HERMITE_CUBIC, PATHFINDER_SAMPLES_HIGH, 0.001, 15.0, 10.0, 60.0, &candidate);
+
+    int length = candidate.length;
+    Segment *trajectory = (Segment *)malloc(length * sizeof(Segment));
+    
+    pathfinder_generate(&candidate, trajectory);
+    
+    Segment *leftTrajectory = (Segment*)malloc(sizeof(Segment) * length);
+    Segment *rightTrajectory = (Segment*)malloc(sizeof(Segment) * length);
+    
+    double wheelbase_width = 0.6;
+
+    pathfinder_modify_tank(trajectory, length, leftTrajectory, rightTrajectory, wheelbase_width);
+
+    free(trajectory);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
